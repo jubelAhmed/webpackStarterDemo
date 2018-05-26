c76831954a9c7e0988ed49d13812f1392f94334f
jubelAhmed <jubel35-1648@diu.edu.bd>
2018-05-26 07:38:12 +0600

    webpack first boilerplate

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../styles/index.scss';
+
+console.log('jjjjj');
+import mo1 from '../images/m1.jpg';
+
+var pix1 = document.getElementById('mo1');
+pix1.src = mo1;
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+module.exports = {
+    entry : './src/js/index.js',
+
+    output : {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist'),
+        publicPath: "/dist" 
+    },
+
+    mode: 'development',
+
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,
+                            removeComments: true,
+                            collapseWhitespace: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+           
+        }),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
+}
